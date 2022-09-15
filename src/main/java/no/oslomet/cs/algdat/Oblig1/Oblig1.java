1e7c336dd7a1caa20d7150d24fa150e9f7824940
gianmk <gianmk6@gmail.com>
2022-09-15 14:57:00 +0200

    oppgave 3 gjort og testet.

--- src/main/java/no/oslomet/cs/algdat/Oblig1/Oblig1.java
+++ src/main/java/no/oslomet/cs/algdat/Oblig1/Oblig1.java
@@ -83,7 +83,7 @@ public class Oblig1 {
         int ulike = 0;
 
         if(a.length == 0) {                                 //sjekker om array er tomt
-            ulike = 0;
+            return 0;
         }
         for(int i = 0; i < a.length;i++){
             boolean ikkeUlike = false;
@@ -106,13 +106,13 @@ public class Oblig1 {
 
     ///// Oppgave 4 //////////////////////////////////////
     public static void delsortering(int[] a) {
-        //throw new UnsupportedOperationException();}
+
       if(a.length==0){
         return;
     }
     int partall = 0;
     int oddetall = 0;
-    //lokke for a sjekke om d kun r enten partall eller oddetall
+
         for(int i = 0; i<a.length; i++){
         if(a[i]%2==0){
             partall++;
@@ -124,8 +124,17 @@ public class Oblig1 {
 
     ///// Oppgave 5 //////////////////////////////////////
     public static void rotasjon(char[] a) {
-        throw new UnsupportedOperationException();
-    }
+
+            char rotere = a[a.length - 1];
+            for (int i = a.length - 1; i > 0; i--) {
+                a[i] = a[i - 1];
+
+            }
+            a[0] = rotere;
+        }
+
+
+
 
     ///// Oppgave 6 //////////////////////////////////////
     public static void rotasjon(char[] a, int k) {
