a90e249fd8177a3377ea650a5e60ec3b00a125c0
gianmk <gianmk6@gmail.com>
2022-09-15 19:57:48 +0200

    oppgave beskrivelse, og litt små ting. ferdig

--- src/test/java/no/oslomet/cs/algdat/Oblig1/Oblig1UnitTest.java
+++ src/test/java/no/oslomet/cs/algdat/Oblig1/Oblig1UnitTest.java
@@ -12,43 +12,43 @@ class Oblig1UnitTest {
 
     @org.junit.jupiter.api.Test
     void maks() {
-        int[] tall = {0,1,2,3,4,5,6,7,8,9};
+        int[] tall = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
         assertEquals(9, Oblig1.maks(tall), "Implementer maks og denne testen");
     }
 
 
     @org.junit.jupiter.api.Test
-        void ombyttinger() {
-            int[] tall = {9,8,7,6,5,4,3,2,1,0};
-            int[] random = Oblig1Test.randPerm(10);
-            assertEquals(9, Oblig1.ombyttinger(tall), "Implementer ombyttinger og denne testen");
-        }
+    void ombyttinger() {
+        int[] tall = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+        int[] random = Oblig1Test.randPerm(10);
+        assertEquals(9, Oblig1.ombyttinger(tall), "Implementer ombyttinger og denne testen");
+    }
 
     @org.junit.jupiter.api.Test
     void antallUlikeSortert() {
-        int[] tall= {3,3,3,3,4,4,5,5,7,8,9};
+        int[] tall = {3, 3, 3, 3, 4, 4, 5, 5, 7, 8, 9};
         assertEquals(6, Oblig1.antallUlikeSortert(tall), "Implementer antallUlikeSortert og denne testen");
     }
 
     @org.junit.jupiter.api.Test
     void antallUlikeUsortert() {
-        int[] tall = {1,2,3,4,4,4};
+        int[] tall = {1, 2, 3, 4, 4, 4};
         assertEquals(4, Oblig1.antallUlikeSortert(tall), "Implementer antallUlikeUsortert og denne testen");
     }
 
     @org.junit.jupiter.api.Test
     void delsortering() {
         int[] forventer = {1, 3, 5, 7, 9, 2, 4, 6, 8, 10};
-        int[] a = {6,10,9,4,1,3,8,5,2,7};
+        int[] a = {6, 10, 9, 4, 1, 3, 8, 5, 2, 7};
         Oblig1.delsortering(a);
-        assertEquals(Arrays.toString(forventer), Arrays.toString(a) , "Implementer delsortering og denne testen");
+        assertEquals(Arrays.toString(forventer), Arrays.toString(a), "Implementer delsortering og denne testen");
     }
 
     @org.junit.jupiter.api.Test
     void rotasjon() {
-        char [] bokstav = {'H','O','A','N','G'};
+        char[] bokstav = {'H', 'O', 'A', 'N', 'G'};
         Oblig1.rotasjon(bokstav);
-        char[] rotertBokstav = {'G','H','O','A','N'};
+        char[] rotertBokstav = {'G', 'H', 'O', 'A', 'N'};
         assertEquals(Arrays.toString(rotertBokstav), Arrays.toString(bokstav), "Implementer rotasjon og denne testen");
     }
 
@@ -57,26 +57,7 @@ class Oblig1UnitTest {
         String a = "ACEG";
         String b = "BDFH";
         String c = "";
-        assertEquals("ABCDEFGH", Oblig1.flett(a,b), "Implementer flett og denne testen");
-        assertEquals("ABCDEFGH", Oblig1.flett(a,b,c), "Implementer flett og denne testen");
-    }
-    @org.junit.jupiter.api.Test
-    void indekssortering() {
-        assertEquals(true, false, "Implementer indekssortering og denne testen");
-    }
-
-    @org.junit.jupiter.api.Test
-    void tredjeMin() {
-        assertEquals(true, false, "Implementer tredjeMin og denne testen");
-    }
-
-    @org.junit.jupiter.api.Test
-    void bokstavNr() {
-        assertEquals(true, false, "Implementer bokstavNr og denne testen");
-    }
-
-    @org.junit.jupiter.api.Test
-    void inneholdt() {
-        assertEquals(true, false, "Implementer inneholdt og denne testen");
+        assertEquals("ABCDEFGH", Oblig1.flett(a, b), "Implementer flett og denne testen");
+        assertEquals("ABCDEFGH", Oblig1.flett(a, b, c), "Implementer flett og denne testen");
     }
 }
